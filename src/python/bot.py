8ee1c405d5d430de20bf8fa3a5f072529575cc07
PG <changux@users.noreply.github.com>
2023-07-22 23:21:13 -0500

    Add files via upload

--- src/python/bot.py
+++ src/python/bot.py
@@ -1,19 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 import numpy as np
 
 # Function to create random user names
 def create_random_uname():
-    len = np.random.randint(6, 12)
-    uname = ''.join(chr(np.random.randint(97, 123)) for _ in range(len))
+    length = np.random.randint(6, 12)
+    uname = ''.join(chr(np.random.randint(97, 123)) for _ in range(length))
     return uname
-
 
 # Toggle to run the bot on a website that has AFD enabled or not
 afd_enabled = True
 # afd_enabled = False
 
-driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
+chrome_options = Options()
+# Add any necessary options here if needed
+# chrome_options.add_argument("--headless")  # Uncomment this line if you want to run Chrome in headless mode
+chromedriver_path = "/Users/username/Downloads/chrome-mac-arm64/chromedriver.app"
+
+driver = webdriver.Chrome(options=chrome_options)
 
 url = ''
 if afd_enabled:
@@ -24,24 +30,24 @@ else:
 # run the bot 3 times and create 3 accounts
 for i in range(3):
     # open new tab after first registration
-	if i > 0:
+    if i > 0:
         driver.execute_script("window.open('');")
         driver.switch_to.window(driver.window_handles[i])
 
     driver.get(url)
     uname = create_random_uname()
 
-    user = driver.find_element_by_id('basic_username')
+    user = driver.find_element(By.ID, 'basic_username')
     user.send_keys(uname)
     time.sleep(1)
 
-    pw = driver.find_element_by_id('basic_password')
+    pw = driver.find_element(By.ID, 'basic_password')
     pw.send_keys('user123')
     time.sleep(1)
 
-    email = driver.find_element_by_id('basic_email')
+    email = driver.find_element(By.ID, 'basic_email')
     email.send_keys(uname + '@example.com')
-    s = max(.5, np.random.normal(2, .5))
+    s = max(0.5, np.random.normal(2, 0.5))
     time.sleep(s)
 
     xpath = ''
@@ -49,6 +55,7 @@ for i in range(3):
         xpath = '/html/body/div/section/main/div/div/div[2]/div/form/div[4]/div/div/div/button'
     else:
         xpath = '/html/body/div/section/main/div/div/div/div/form/div[4]/div/div/div/button'
-    driver.find_element_by_xpath(xpath).click()
-    time.sleep(2)
+    driver.find_element(By.XPATH, xpath).click()
+    time.sleep(60)
 
+driver.quit()
